c69d0efc35b68aaf43119884d38603539e3c2e3e
doviss <dovydasp@protonmail.ch>
2020-09-12 11:56:56 +0300

    Started project

---
+++ README.md
@@ -0,0 +1,20 @@
+# MCStatusBot
+Discord bot that shows your Minecraft server's status.
+## How to set up
+### Making the bot
+1. Go to https://discord.com/developers/applications
+2. Login and click "New Application" positioned top right of the screen
+3. Enter the name of the bot (your choice) and click "create"
+4. Once you're on your bots page click "Bot" positioned on the left, click "Add Bot" on the right, then click "Yes, do it!"
+### Preparing the code
+1. On your text editor of choice open the "mcserverbot.py" file
+2. On lines 5, 6, 7 enter required information in the quatations (Bot token can be found in https://discord.com/developers/applications/*YOURBOTID*/bot (Do not share token!))
+### Adding the bot to your server
+1. In your applications page click OAuth2
+2. In "Scopes" field select "bot"
+3. In "Bot permissions" field select "Administrator"
+4. Open the link that was generated above
+### Running the bot
+1. Install [Python3](https://www.python.org/downloads/)
+2. In Command Prompt type in "py -3 -m pip install -U discord.py"
+3. Double click the "mcserverbot.py" file
---
+++ mcserverbot.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+import requests
+
+PREFIX = '.' # default prefix is .
+SERVER_IP = 'Enter your server here'
+TOKEN = 'Enter bot token here'
+
+client = commands.Bot(command_prefix = PREFIX)
+
+@client.event
+async def on_ready():
+	print('Bot is running.')
+
+@client.command()
+async def status(ctx):
+	r = requests.get('https://api.mcsrvstat.us/2/' + SERVER_IP)
+	json_data = r.json()
+
+	motd = str(json_data["motd"]["html"])
+	players = str(json_data["players"]["online"])
+	maxPlayers = str(json_data["players"]["max"])
+	onlineBool = str(json_data["online"])
+	version = str(json_data["version"])
+
+	if onlineBool == 'True':
+		online = 'Online'
+	else:
+		online = 'Offline'
+
+	embed=discord.Embed(title="Server Status", color=0x00ff00)
+	embed.set_thumbnail(url='https://api.mcsrvstat.us/icon/' + SERVER_IP)		
+	embed.add_field(name="Status", value=online, inline=True)
+	embed.add_field(name="Player Count", value=players + '/' + maxPlayers, inline=True)
+	embed.add_field(name="Version", value=version, inline=True)
+	embed.add_field(name="MOTD", value=motd, inline=True)
+
+	await ctx.send(embed=embed)
+
+client.run(TOKEN)
